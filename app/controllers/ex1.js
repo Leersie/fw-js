7feddc61a7feb4dbbb6ec337608ac143fe3e6252
22005397 <22005397@etu.unicaen.fr>
2022-01-26 16:51:15 +0100

    third commit

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,5 +1,33 @@
 import Controller from '@ember/controller';
-import {action} from '@ember/object';
-import {tracked} from "@glimmer/tracking";
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class Ex1Controller extends Controller {}
+export default class Ex1Controller extends Controller {
+  @tracked content = '';
+  MAX = 100;
+  @tracked info = '';
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+  get style() {
+    if(this.size < 15){
+        return 'danger';
+    } else if(this.size < 50){
+      return 'warning';
+    } else{
+      return 'info';
+    }
+  }
+
+  @action update() {
+    this.info = 'Note modifiée';
+  }
+  @action save() {
+    this.info = 'Note sauvegardée';
+  }
+
+  @action clear() {
+    this.content = '';
+  }
+}
